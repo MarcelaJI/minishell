d353d76914ff7cb972b409261c40c10463a3e9ab
MarcelaJI <ingridjimenez113@gmail.com>
2025-06-24 10:59:50 +0200

    Adding

---
+++ includes/minishell.h
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ingjimen <ingjimen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/24 10:51:55 by ingjimen          #+#    #+#             */
+/*   Updated: 2025/06/24 10:58:12 by ingjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../Libft/libft.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <errno.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include <sys/wait.h>
+
+# define RED		"\x1b[31m"
+# define GREEN		"\x1b[32m"
+# define YELLOW		"\x1b[33m"
+# define BLUE		"\x1b[34m"
+# define MAGENTA	"\x1b[35m"
+# define CYAN		"\x1b[36m"
+# define WHITE		"\x1b[0;37m"
+# define BOLD		"\x1b[1m"
+# define RESET		"\x1b[0m"
+# define SHELLNAME	"[shellmi]: "
+# define SHELLPRMPT	"[shellmi] "
+
+/* FLAGS FOR TOKEN TYPES */
+enum e_type
+{
+	CMD = 0,
+	PARAM = 1,
+	PIPE = 2,
+	FILENAME = 3,
+	INF = 4,
+	OUTF = 5,
+	OUTF_APD = 6,
+	HERE_DOC = 7,
+	LIMITER = 8,
+	UNKNOWN = 9,
+	NA = -1
+};
+
+/* DOUBLY LINKED LIST STRUCTURE */
+typedef struct s_dlist
+{
+	char			*str;
+	enum e_type		token;
+	int				index;
+	struct s_dlist	*next;
+	struct s_dlist	*prev;
+}	t_dlist;
+
+typedef struct s_data
+{
+    t_dlist	*tokens;
+}   t_data;
+
+
+
+#endif
---
+++ src/tokenizer.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ingjimen <ingjimen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/24 10:35:22 by ingjimen          #+#    #+#             */
+/*   Updated: 2025/06/24 10:49:04 by ingjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+
+int validate_token_syntax(t_data *data)
+{
+    t_dlist *temp = data->tokens;
+
+    while (temp)
+    {
+        if ((is_redirection_token(temp) || temp->token == HERE_DOC) && !temp->next)
+            return error("syntax error near unexpected token: ", "\'newline\'", data);
+        if (((temp->index == 0 || temp->index == listSize(data->tokens) - 1) && (temp->token == PIPE)) ||
+            (temp->token == PIPE && temp->next && temp->next->token == PIPE) ||
+            (is_redirection_token(temp) && is_redirection_token(temp->next)) ||
+            (temp->token == HERE_DOC && temp->next && temp->next->token == HERE_DOC) ||
+            (temp->token == HERE_DOC && temp->next && temp->next->token != LIMITER) ||
+            ((is_redirection_token(temp) || temp->token == HERE_DOC) && temp->next && temp->next->token == PIPE))
+            return error("syntax error near unexpected token: ", temp->str, data);
+        if (temp->token == UNKNOWN)
+            return error("unrecognized token: ", temp->str, data);
+        temp = temp->next;
+    }
+    return 0;
+}
